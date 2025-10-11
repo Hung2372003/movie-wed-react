98ede4d0fcc52e995349ab396deebd4df23c3d32
Hung <hung110195@gmail.com>
2025-10-12 01:53:53 +0700

    ok

--- src/components/preloader/preloader.component.tsx
+++ src/components/preloader/preloader.component.tsx
@@ -3,13 +3,9 @@ import React from "react";
 const PreloaderComponent: React.FC = () => {
   return (
     <div id="preloader">
-      <img
-        className="logo"
-        src="/images/logo1.png"
-        alt="Logo"
-        width={119}
-        height={58}
-      />
+    <div className="logoWrap">
+                <h1 className="logo">BeuFilm</h1>
+              </div>
       <div id="status">
         <span></span>
         <span></span>
